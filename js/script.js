55cd5cdd8ce08363eb1d0615da2a5139e9917cd7
cksgud22 <cksgud0070@gmail.com>
2024-07-29 15:29:09 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function () {
+
+    $(".main>li").hover(function () {
+        // $(this).find(".sub").stop().slideDown();
+        $(this).children(".sub").stop().slideDown();
+    }, function () {
+        $(".sub").stop().slideUp();
+    }) //hover
+
+
+    // $(".main>li").mouseover(function(){
+    //     $(this).children(".sub").stop().slideDown();
+    // })//over
+    // $(".main>li").mouseout(function(){
+    //     $(".sub").stop().slideUp();
+    // })//out
+
+
+    //이미지 슬라이드
+    var n = 0;
+
+    setInterval(function () {
+        n = (n + 1) % 3;
+
+        // if(n==2){
+        //     n=0;
+        // }else{
+        //     n++;
+        // }
+
+        console.log(n);
+
+        $(".left_move").animate({left :n * (-100) + "%"} , 500)
+    }, 3000)
+
+
+    $(".p_click").click(function(){
+        $(".popup, .modal").show();
+    })
+    $(".close").click(function(){
+        $(".popup, .modal").hide();
+    })
+
+
+}) //jquery
